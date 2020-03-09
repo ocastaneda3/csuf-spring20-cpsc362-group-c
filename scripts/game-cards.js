959ebf7924544d116367cd020d694f9c27fdf2ae
Oscar Castaneda <ocastaneda3@csu.fullerton.edu>
2020-03-09 01:16:54 -0700

    Clean code up from previous push

--- scripts/game-cards.js
+++ scripts/game-cards.js
@@ -42,10 +42,57 @@ const prevPage = () => {
 	console.log('Prev: ' + page_count.value);
 };
 
-const printGameTtitle = () => {
-	console.log(this.textContent);
-};
+const getPlatformsList = (game, platforms_list) => {
+	game.parent_platforms.forEach(x => {
+		const new_platform = document.createElement('div');
 
+		switch (x.platform.name) {
+			case 'PC':
+				new_platform.className = 'platforms platform__medium platform__pc';
+				new_platform.style.backgroundImage = 'url(./images/platform_pc_white_logo.svg';
+				break;
+			case 'PlayStation':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__playstation';
+				new_platform.style.backgroundImage = 'url(./images/platform_playstation_white_logo.svg';
+				break;
+			case 'Xbox':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__xbox';
+				new_platform.style.backgroundImage = 'url(./images/platform_xbox_white_logo.svg';
+				break;
+			case 'Apple Macintosh':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__mac';
+				new_platform.style.backgroundImage = 'url(./images/platform_mac_white_logo.svg';
+				break;
+			case 'Linux':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__linux';
+				new_platform.style.backgroundImage = 'url(./images/platform_linux_white_logo.svg';
+				break;
+			case 'Nintendo':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__nintendo';
+				new_platform.style.backgroundImage = 'url(./images/platform_nintendo_white_logo.svg';
+				break;
+			case 'iOS':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__ios';
+				new_platform.style.backgroundImage = 'url(./images/platform_ios_white_logo.svg';
+				break;
+			case 'Android':
+				// console.log(x.platform.name);
+				new_platform.className = 'platforms platform__medium platform__android';
+				new_platform.style.backgroundImage = 'url(./images/platform_android_white_logo.svg';
+				break;
+			default:
+				console.log('Need Logo '.concat(x.platform.name));
+				break;
+		}
+		platforms_list.appendChild(new_platform);
+	});
+};
 
 /************************************************/
 /* Function: sendHttpRequest                    */
@@ -65,68 +112,22 @@ const sendHttpRequest = (method, url) => {
 			game_card.setAttribute('class', 'card border-secondary mb-3');
 	
 			// Get Game Image
-			const img = document.createElement('img');
-			img.className = 'background';
-			img.src = game.background_image;
+			const game_img = document.createElement('img');
+			game_img.className = 'background';
+			game_img.src = game.background_image;
 
 			// Get Platforms
-			const platforms = document.createElement('div');
-			platforms.setAttribute('class', 'platforms');
+			const platforms_list = document.createElement('div');
+			// platforms.setAttribute('class', 'platforms');
+			platforms_list.className = 'platforms';
 
-			game.parent_platforms.forEach(x => {
-				const new_platform = document.createElement('div');
-
-				switch (x.platform.name) {
-					case 'PC':
-						new_platform.className = 'platforms platform__medium platform__pc';
-						new_platform.style.backgroundImage = 'url(./images/platform_pc_white_logo.svg';
-						break;
-					case 'PlayStation':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__playstation';
-						new_platform.style.backgroundImage = 'url(./images/platform_playstation_white_logo.svg';
-						break;
-					case 'Xbox':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__xbox';
-						new_platform.style.backgroundImage = 'url(./images/platform_xbox_white_logo.svg';
-						break;
-					case 'Apple Macintosh':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__mac';
-						new_platform.style.backgroundImage = 'url(./images/platform_mac_white_logo.svg';
-						break;
-					case 'Linux':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__linux';
-						new_platform.style.backgroundImage = 'url(./images/platform_linux_white_logo.svg';
-						break;
-					case 'Nintendo':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__nintendo';
-						new_platform.style.backgroundImage = 'url(./images/platform_nintendo_white_logo.svg';
-						break;
-					case 'iOS':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__ios';
-						new_platform.style.backgroundImage = 'url(./images/platform_ios_white_logo.svg';
-						break;
-					case 'Android':
-						// console.log(x.platform.name);
-						new_platform.className = 'platforms platform__medium platform__android';
-						new_platform.style.backgroundImage = 'url(./images/platform_android_white_logo.svg';
-						break;
-					default:
-						console.log('Need Logo '.concat(x.platform.name));
-						break;
-				}
-				platforms.appendChild(new_platform);
-			});
+			getPlatformsList(game, platforms_list);
 
 			// Get Game Name
-			const h1 = document.createElement('div');
-			h1.setAttribute('class', 'card-header')
-			h1.textContent = game.name;
+			const game_title = document.createElement('div');
+
+			game_title.setAttribute('class', 'card-header')
+			game_title.textContent = game.name;
 			game_card.title = game.slug;
 
 			// Set onClick Function
@@ -136,9 +137,10 @@ const sendHttpRequest = (method, url) => {
 				window.location.href = './game.html?title='.concat(this.title);
 			};
 			
-			game_card.appendChild(img);
-			game_card.appendChild(platforms);
-			game_card.appendChild(h1);
+			game_card.appendChild(game_img);
+			game_card.appendChild(platforms_list);
+			game_card.appendChild(game_title);
+
 			container.appendChild(game_card);
 		});
 	};
