ba9901b06dde4356267849032a81f0f9a1b1e6f4
Oscar Castaneda <ocastaneda3@csu.fullerton.edu>
2020-03-09 01:03:09 -0700

    Added platform icons for each game

--- scripts/game-cards.js
+++ scripts/game-cards.js
@@ -64,23 +64,80 @@ const sendHttpRequest = (method, url) => {
 			const game_card = document.createElement('div');
 			game_card.setAttribute('class', 'card border-secondary mb-3');
 	
+			// Get Game Image
+			const img = document.createElement('img');
+			img.className = 'background';
+			img.src = game.background_image;
+
+			// Get Platforms
+			const platforms = document.createElement('div');
+			platforms.setAttribute('class', 'platforms');
+
+			game.parent_platforms.forEach(x => {
+				const new_platform = document.createElement('div');
+
+				switch (x.platform.name) {
+					case 'PC':
+						new_platform.className = 'platforms platform__medium platform__pc';
+						new_platform.style.backgroundImage = 'url(./images/platform_pc_white_logo.svg';
+						break;
+					case 'PlayStation':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__playstation';
+						new_platform.style.backgroundImage = 'url(./images/platform_playstation_white_logo.svg';
+						break;
+					case 'Xbox':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__xbox';
+						new_platform.style.backgroundImage = 'url(./images/platform_xbox_white_logo.svg';
+						break;
+					case 'Apple Macintosh':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__mac';
+						new_platform.style.backgroundImage = 'url(./images/platform_mac_white_logo.svg';
+						break;
+					case 'Linux':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__linux';
+						new_platform.style.backgroundImage = 'url(./images/platform_linux_white_logo.svg';
+						break;
+					case 'Nintendo':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__nintendo';
+						new_platform.style.backgroundImage = 'url(./images/platform_nintendo_white_logo.svg';
+						break;
+					case 'iOS':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__ios';
+						new_platform.style.backgroundImage = 'url(./images/platform_ios_white_logo.svg';
+						break;
+					case 'Android':
+						// console.log(x.platform.name);
+						new_platform.className = 'platforms platform__medium platform__android';
+						new_platform.style.backgroundImage = 'url(./images/platform_android_white_logo.svg';
+						break;
+					default:
+						console.log('Need Logo '.concat(x.platform.name));
+						break;
+				}
+				platforms.appendChild(new_platform);
+			});
+
 			// Get Game Name
 			const h1 = document.createElement('div');
 			h1.setAttribute('class', 'card-header')
 			h1.textContent = game.name;
 			game_card.title = game.slug;
+
+			// Set onClick Function
 			game_card.onclick = function(){
 				// console.log(String(this.textContent).split(' ').join('-').toLocaleLowerCase())
 				console.log(this.title);
 				window.location.href = './game.html?title='.concat(this.title);
 			};
 			
-			// Get Game Image
-			const img = document.createElement('img');
-			img.setAttribute('class', 'background');
-			img.src = game.background_image;
-	
 			game_card.appendChild(img);
+			game_card.appendChild(platforms);
 			game_card.appendChild(h1);
 			container.appendChild(game_card);
 		});
@@ -88,7 +145,7 @@ const sendHttpRequest = (method, url) => {
 	request.send();
 };
 
-const app = document.getElementById('root');
+const app = document.getElementById('game-card-list');
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
