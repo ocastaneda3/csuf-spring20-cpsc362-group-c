992544845c18997ad819c9d4acd68e9456f8f38e
Cheng No Chang <cjerry80@csu.fullerton.edu>
2020-05-06 16:11:03 -0700

    Input data into database using php

---
+++ reviewlogin.php
@@ -0,0 +1,31 @@
+<?php
+
+$dBServername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+$dBName = "login";
+
+$conn = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
+
+if(!$conn){
+    echo 'Not connected to Server';
+}
+
+if(!mysqli_select_db($conn,'login')){
+    echo 'Database not selected';
+}
+
+//$Game_id= $_POST['game_title'];
+$Review = $_POST['review'];
+$Rating = $_POST['rating'];
+
+$sql = "INSERT INTO user_reviews (review, rating) VALUES ( '$Review', '$Rating')";
+
+if(mysqli_query($conn,$sql)){
+    echo 'Review Inserted';
+    header("Refresh:1; url=game-list.php");
+}else{
+    echo mysqli_error($conn);
+}	
+header("Refresh");
+?>
